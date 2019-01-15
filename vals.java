3f2b3385f436dba6d14d994ab87d9bed4b3020bc
koolguy421 <44180066+koolguy421@users.noreply.github.com>
2019-01-14 19:40:26 -0600

    Create vals.java

---
+++ vals.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode;
+
+
+public interface vals {
+    /* Constants */
+    double SPIN_SPEED = 0.6;
+    double SLOW_SPEED = 0.4;
+    double FAST_SPEED = 0.75;
+    double EXTEND_SPEED = 0.8;
+}
