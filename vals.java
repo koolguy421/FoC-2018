33ed04603b1e97d283fa5a4285bdd3ee49ec7280
koolguy421 <44180066+koolguy421@users.noreply.github.com>
2019-01-26 01:07:36 -0600

    Update vals.java

--- vals.java
+++ vals.java
@@ -1,10 +1,17 @@
 package org.firstinspires.ftc.teamcode;
 
 
-public interface vals {
+public interface vals
+{
     /* Constants */
-    double SPIN_SPEED = 0.6;
-    double SLOW_SPEED = 0.4;
-    double FAST_SPEED = 0.75;
-    double EXTEND_SPEED = 0.8;
+    double SPIN_SPEED = 1;
+    double SLOW_SPEED = 0.2;
+    double FACT = 0.4;
+    double FAST_SPEED = 0.8;
+    double EXTEND_SPEED = 1.0;
+    int MAX_POS_LIFT = 1000;
+    double MAX_POS_EXTEND = 90;
+
+
 }
+
